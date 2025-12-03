267428e5072a5e1ebc9e06bdba906837c4381e63
Destriluk <abasaariho794@gmail.com>
2025-12-03 14:40:24 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("this is python operators" )
+num1 = 78
+num2 = 56
+num3 = 45
+
+ans = 0
+ans = num1 + num2 + num3
+print("the sum is;",ans)
+ans = num2 - num1 - num3
+print("the difference is :",ans)
+ans = num1 * num3 * num2
+print("the result is :",ans)
+ans = num1 / num2 / num3
+print("the dividend is:",ans)
